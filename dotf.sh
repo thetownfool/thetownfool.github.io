cffd9f2eb2929202a0d253d17e03cbe98dfee4b7
thetownfool <thetownfool@gmail.com>
2021-01-20 23:08:05 +0000

    Update dotf.sh

--- dotf.sh
+++ dotf.sh
@@ -1,13 +1,1 @@
-<!DOCTYPE HTML>                                                                 
-<html lang="en">                                                                
-    <head>                                                                      
-        <meta charset="utf-8">
-        <meta http-equiv="refresh" content="0;url=https://git.io/Jtm53" />       
-        <link rel="canonical" href="https://git.io/Jtm53" />                     
-    </head>                                                                                                                                                                   
-    <body>                                                                      
-        <h1>                                                                    
-            The page been moved to <a href="https://git.io/Jtm53">https://git.io/Jtm53</a>
-        </h1>                                                                   
-    </body>                                                                     
-</html>
+wget https://git.io/Jtm53
